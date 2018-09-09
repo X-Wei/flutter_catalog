5b84d3d37d5a6c89b737203590bee50255b49e90
x-wei <wistar.sjtu@gmail.com>
2018-09-09 19:49:10 +0200

    upd container example (add decoration examples)

--- lib/routes/layouts_container_padding_center_ex.dart
+++ lib/routes/layouts_container_padding_center_ex.dart
@@ -32,7 +32,11 @@ class ContainerBasicsExample extends MyRoute {
           // EdgeInsets.all: same padding value for all 4 directions.
           padding: const EdgeInsets.all(16.0),
           child: Container(
-            color: Colors.yellow,
+            // Container.color should not be set when decoration is set.
+            decoration: BoxDecoration(
+                border: Border.all(width: 5.0, color: Colors.black),
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(20.0)),
             // Container.padding is implementd internally with Padding widgets.
             // EdgeInsets.fromLTRB: specify padding for left/right/top/bottom.
             padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
@@ -41,7 +45,13 @@ class ContainerBasicsExample extends MyRoute {
               // EdgeInsets.symmetric: specify vertical/horizontal padding.
               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Container(
-                color: Colors.green,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(10.0),
+                    right: Radius.circular(30.0),
+                  ),
+                ),
               ),
             ),
           ),
