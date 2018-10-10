bdd85490c1f6884999409f33a8686c8d8590a955
x-wei <wistar.sjtu@gmail.com>
2018-10-10 15:06:59 +0200

    add more complex card example

--- lib/routes/widgets_card_ex.dart
+++ lib/routes/widgets_card_ex.dart
@@ -9,7 +9,8 @@ class CardExample extends MyRoute {
   get title => 'Card, Inkwell';
 
   @override
-  get description => 'Container with corner and shadow; inkwell (ripple) effects';
+  get description =>
+      'Container with corner and shadow; inkwell (ripple) effects';
 
   @override
   get links => {
@@ -19,7 +20,8 @@ class CardExample extends MyRoute {
 
   @override
   Widget buildMyRouteContent(BuildContext context) {
-    return Column(
+    return ListView(
+      padding: EdgeInsets.all(8.0),
       children: <Widget>[
         Card(
           color: Colors.red,
@@ -68,10 +70,61 @@ class CardExample extends MyRoute {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Text('Card 3'),
+                  child: Text('Card 3 (with custom border radius)'),
                 ),
               ],
             ),
+          ),
+        ),
+        Card(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 180.0,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image.asset(
+                        'res/images/material_design_4.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 16.0,
+                      left: 16.0,
+                      right: 16.0,
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Card 4 (complex example)',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              ButtonTheme.bar(
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('SHARE'),
+                      onPressed: () {},
+                    ),
+                    FlatButton(
+                      child: Text('EXPLORE'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ],
