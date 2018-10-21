711c5535c099bff57e6d8e383647de21e3d279aa
x-wei <wistar.sjtu@gmail.com>
2018-10-21 20:06:34 +0200

    Move navitems to home page (prototype, still WIP)

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -14,11 +14,75 @@ class MyHomeRoute extends MyRoute {
 
   @override
   Widget buildMyRouteContent(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Screen',
-        style: Theme.of(context).textTheme.title,
-      ),
+    // TODO: populate listview from kMyAppRoutesStructure.
+    return ListView(
+      children: <Widget>[
+        Card(
+          child: ExpansionTile(
+            leading: Icon(Icons.widgets),
+            title: Text(
+              'Widgets',
+              style: Theme.of(context).textTheme.title,
+            ),
+            children: <Widget>[
+              ListTile(
+                trailing: Icon(Icons.keyboard_arrow_right),
+                title: Text('IconExample'),
+                onTap: () => Navigator.of(context).pushNamed('/IconExample'),
+              ),
+              ListTile(
+                trailing: Icon(Icons.keyboard_arrow_right),
+                title: Text('TextExample'),
+                onTap: () => Navigator.of(context).pushNamed('/TextExample'),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: ExpansionTile(
+            leading: Icon(Icons.layers),
+            title: Text(
+              'Layout',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ),
+        Card(
+          child: ExpansionTile(
+            leading: Icon(Icons.list),
+            title: Text(
+              'Lists',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ),
+        Card(
+          child: ExpansionTile(
+            leading: Icon(Icons.menu),
+            title: Text(
+              'AppBar',
+              style: Theme.of(context).textTheme.title,
+            ),
+            children: <Widget>[
+              ListTile(
+                trailing: Icon(Icons.keyboard_arrow_right),
+                title: Text('SliverAppBarExample'),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/SliverAppBarExample'),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: ExpansionTile(
+            leading: Icon(Icons.view_carousel),
+            title: Text(
+              'Navigation',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
