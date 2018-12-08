0228123fa7b0b4b26d40dd5865198b4bb6fd0650
x-wei <wistar.sjtu@gmail.com>
2018-12-08 18:00:56 +0100

    remove unused constants

--- lib/routes/firebase_chatroom_ex.dart
+++ lib/routes/firebase_chatroom_ex.dart
@@ -2,13 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:async';
 import './firebase_constants.dart';
 import '../my_route.dart';
 
-final kFirebaseAuth = FirebaseAuth.instance;
-final kGoogleSignIn = GoogleSignIn();
 
 // NOTE: to add firebase support, first go to firebase console, generate the
 // firebase json file, and add configuration lines in the gradle files.
