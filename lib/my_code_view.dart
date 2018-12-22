eeb7e6f9371fe324f8f59225a4a81c795a7b8ae1
x-wei <wistar.sjtu@gmail.com>
2018-12-22 22:14:10 +0100

    add ability to change code view font (by two-finger scale)

--- lib/my_code_view.dart
+++ lib/my_code_view.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './syntax_highlighter.dart';
 
-class MyCodeView extends StatelessWidget {
+class MyCodeView extends StatefulWidget {
   final String filePath;
 
   MyCodeView({@required this.filePath});
+
+  @override
+  MyCodeViewState createState() {
+    return new MyCodeViewState();
+  }
+}
+
+class MyCodeViewState extends State<MyCodeView> {
+  double _textScaleFactor = 1.0;
 
   Widget _getCodeView(String codeContent, BuildContext context) {
     final SyntaxHighlighterStyle style =
@@ -15,17 +24,22 @@ class MyCodeView extends StatelessWidget {
     // TODO: try out CustomScrollView and SliverAppbar (appbar auto hides when scroll).
     return Scrollbar(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: SingleChildScrollView(
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: RichText(
-                text: TextSpan(
-                    style: const TextStyle(
-                        fontFamily: 'monospace', fontSize: 11.0),
-                    children: <TextSpan>[
-                  DartSyntaxHighlighter(style).format(codeContent)
-                ])),
+            child: GestureDetector(
+              onScaleUpdate: (ScaleUpdateDetails details) {
+                setState(() => this._textScaleFactor = details.scale);
+              },
+              child: RichText(
+                  textScaleFactor: this._textScaleFactor,
+                  text: TextSpan(
+                      style: TextStyle(fontFamily: 'monospace', fontSize: 12.0),
+                      children: <TextSpan>[
+                        DartSyntaxHighlighter(style).format(codeContent)
+                      ])),
+            ),
           ),
         ),
       ),
@@ -37,7 +51,7 @@ class MyCodeView extends StatelessWidget {
     // Loading string from file returns a Future<String>, so instead of returning directly the
     // widget, we need a FutureBuilder.
     return FutureBuilder(
-      future: DefaultAssetBundle.of(context).loadString(filePath) ??
+      future: DefaultAssetBundle.of(context).loadString(widget.filePath) ??
           'Error loading source code from $this.filePath',
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         if (snapshot.hasData) {
