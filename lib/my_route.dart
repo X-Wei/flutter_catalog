b5ff511469f27cbac47fc2f9ab942a38807ef05e
x-wei <wistar.sjtu@gmail.com>
2018-12-23 20:36:18 +0100

    Demo stars: use rootAncestorStateOfType and expose _MyRouteState Move most of the logic into my_route.dart Still has problems with updating widgets

--- lib/my_route.dart
+++ lib/my_route.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
@@ -11,10 +12,18 @@ abstract class MyRoute extends StatefulWidget {
   // Preference key's prefix for storing whether you have stared a route or not.
   static const kStaredPreferenceKeyPrefx = 'StaredFor_';
 
+  static of(BuildContext context) {
+    final ret =
+        context.rootAncestorStateOfType(const TypeMatcher<_MyRouteState>());
+    print('MyRouteState.of returned: ${ret.runtimeType.toString()}');
+    return ret;
+  }
+
   const MyRoute(this._sourceFile);
 
   // Subclasses can return routeName accordingly (polymorphism).
-  String get routeName => '/${this.runtimeType.toString()}';
+  String get routeName => this.runtimeType.toString();
+  // String get routeName => '/${this.runtimeType.toString()}';
 
   // Title shown in the route's appbar and in the app's navigation drawer item.
   // By default just returns routeName.
@@ -33,24 +42,6 @@ abstract class MyRoute extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _MyRouteState();
-
-  // Returns whether this widget is stared.
-  bool isStared(SharedPreferences prefs) {
-    if (prefs==null) {
-      return false;
-    }
-    return prefs.getBool(
-          '$kStaredPreferenceKeyPrefx${this.routeName}',
-        ) ??
-        false;
-  }
-
-  // Toggles the stared/not-stated status of the route.
-  toggleStared(SharedPreferences prefs) {
-    assert(prefs != null);
-    bool stared = isStared(prefs);
-    prefs.setBool('$kStaredPreferenceKeyPrefx${this.routeName}', !stared);
-  }
 }
 
 // Each MyRoute contains two tabs: "Preview" and "Code".
@@ -86,8 +77,31 @@ const _TABS = <Widget>[
 ];
 
 class _MyRouteState extends State<MyRoute> with SingleTickerProviderStateMixin {
+  static const kStaredPreferenceKeyPrefx = 'StaredFor_';
   TabController _tabController;
   SharedPreferences _preferences;
+  // Maps route names to the corresponding _DemoStarsRecord, this map serves as
+  // a local cache of the cloud firestore snapshot.
+  final Map<String, _DemoStarsRecord> demoStars = {};
+
+  // Returns whether routeName (defaults to this.widget.routeName) is stared.
+  bool isStared([String routeName]) {
+    print('isStared: ${routeName ?? this.widget.routeName}');
+    return this._preferences.getBool(
+              '$kStaredPreferenceKeyPrefx${routeName ?? this.widget.routeName}',
+            ) ??
+        false;
+  }
+
+  // Toggles the stared/not-stated status of routeName (defaults to
+  // this.widget.routeName).
+  toggleStared([String routeName]) {
+    print('toggleStared: ${routeName ?? this.widget.routeName}');
+    bool stared = this.isStared();
+    this._preferences.setBool(
+        '$kStaredPreferenceKeyPrefx${routeName ?? this.widget.routeName}',
+        !stared);
+  }
 
   @override
   void initState() {
@@ -110,53 +124,58 @@ class _MyRouteState extends State<MyRoute> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Text(this.widget.title),
-        ),
-        actions: _getAppbarActions(),
-        bottom: TabBar(
-          tabs: _TABS,
-          controller: this._tabController,
-        ),
-      ),
-      // Use a Builder so that Scaffold.of(context) uses correct context, c.f.
-      // https://stackoverflow.com/a/45948913
-      body: Builder(builder: (BuildContext context) {
-        final myTabPages = <Widget>[
-          // "Preview" tab:
-          this.widget.buildMyRouteContent(context),
-          // "Code" tab:
-          MyCodeView(filePath: this.widget._sourceFile),
-        ];
-        assert(myTabPages.length == _TABS.length);
-        // Body of MyRoute is two-tabs ("Preview" and "Code").
-        return TabBarView(
-          children: myTabPages,
-          controller: this._tabController,
-        );
-      }),
-      // Only home route has drawer:
-      drawer: this.widget.routeName == Navigator.defaultRouteName
-          ? Drawer(
-              child: my_app_meta.getNavDrawerItems(this, context),
-            )
-          : null,
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('demo_stars').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _loadDemoStarsFromSnapshot(snapshot);
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(this.widget.title),
+              ),
+              actions: _getAppbarActions(),
+              bottom: TabBar(
+                tabs: _TABS,
+                controller: this._tabController,
+              ),
+            ),
+            // Use a Builder so that Scaffold.of(context) uses correct context, c.f.
+            // https://stackoverflow.com/a/45948913
+            body: Builder(builder: (BuildContext context) {
+              final myTabPages = <Widget>[
+                // "Preview" tab:
+                this.widget.buildMyRouteContent(context),
+                // "Code" tab:
+                MyCodeView(filePath: this.widget._sourceFile),
+              ];
+              assert(myTabPages.length == _TABS.length);
+              // Body of MyRoute is two-tabs ("Preview" and "Code").
+              return TabBarView(
+                children: myTabPages,
+                controller: this._tabController,
+              );
+            }),
+            // Only home route has drawer:
+            drawer: this.widget.routeName == Navigator.defaultRouteName
+                ? Drawer(
+                    child: my_app_meta.getNavDrawerItems(this, context),
+                  )
+                : null,
+          );
+        });
   }
 
   List<Widget> _getAppbarActions() {
+    // final state = MyRouteState.of(context);
     final appbarActions = <Widget>[
       IconButton(
         icon: Icon(
-          this.widget.isStared(this._preferences)
-              ? Icons.star
-              : Icons.star_border,
+          this.isStared() ? Icons.star : Icons.star_border,
         ),
-        onPressed: () =>
-            setState(() => this.widget.toggleStared(this._preferences)),
+        onPressed: () => setState(() => this.toggleStared()),
       ),
     ];
     if (this.widget.links.isNotEmpty) {
@@ -185,4 +204,86 @@ class _MyRouteState extends State<MyRoute> with SingleTickerProviderStateMixin {
     }
     return appbarActions;
   }
+
+  // Caches the value of demo stars from snapshot into this._demoStars.
+  void _loadDemoStarsFromSnapshot(AsyncSnapshot<QuerySnapshot> snapshot) {
+    for (DocumentSnapshot doc in snapshot.data.documents) {
+      final demoStarsRecord = _DemoStarsRecord.fromSnapshot(doc);
+      this.demoStars[demoStarsRecord.routeName] = demoStarsRecord;
+    }
+  }
+
+  // Creates a document corresponding to routeName on cloud firestore.
+  Future<Null> _createDemoStarsFirestoreDocument(String routeName) async {
+    // In firestore, the documents are keyed by corresponding route names.
+    await Firestore.instance
+        .collection('demo_stars')
+        .document(routeName)
+        .setData({'routeName': routeName, 'stars': 0}, merge: true);
+  }
+
+  // Toggles the star status of one record, also updats the star counts in
+  // cloud firestore.
+  // If the corresponding document is not yet created on firestore, create it
+  // first, then re-call this function after 1 second.
+  Future<Null> toggleStaredAsync(MyRoute route) async {
+    _DemoStarsRecord record = this.demoStars[route.routeName];
+    if (record == null) {
+      print('creating record for ${route.routeName}');
+      _createDemoStarsFirestoreDocument(route.routeName)
+        ..then((_) async {
+          // After the document is created, wait for the firestore update to be
+          // synced back -- 1 second should be more than enough.
+          await Future.delayed(Duration(seconds: 1));
+          // Next time this function is called, the record will not be null.
+          toggleStaredAsync(route);
+        });
+      return;
+    }
+    int deltaStars = this.isStared(route.routeName) ? -1 : 1;
+    try {
+      await Firestore.instance.runTransaction((transaction) async {
+        final freshSnapshot =
+            await transaction.get(record.firestoreDocReference);
+        final freshRecord = _DemoStarsRecord.fromSnapshot(freshSnapshot);
+        await transaction.update(record.firestoreDocReference,
+            {'stars': freshRecord.stars + deltaStars});
+      });
+      setState(() {
+        this.toggleStared(route.routeName);
+      });
+    } catch (e) {
+      print('Error doing firebase transaction: $e');
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error doing firebase transaction: $e'),
+        ),
+      );
+    }
+  }
+}
+
+class DemoStarsManager {
+  // static DemoStarsManager of(BuildContext contexFt) =>
+}
+
+// Custom data class for holding "{routeName,stars}" records.
+class _DemoStarsRecord {
+  final String routeName;
+  final int stars;
+  // Reference to this record as a firestore document.
+  final DocumentReference firestoreDocReference;
+
+  _DemoStarsRecord.fromMap(Map<String, dynamic> map,
+      {@required this.firestoreDocReference})
+      : assert(map['routeName'] != null),
+        assert(map['stars'] != null),
+        routeName = map['routeName'],
+        stars = map['stars'];
+
+  _DemoStarsRecord.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, firestoreDocReference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$routeName:$stars>";
 }
--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../my_route.dart';
 import '../my_app_meta.dart'
     show kMyAppRoutesStructure, MyRouteGroup, kAboutRoute;
@@ -28,35 +27,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  SharedPreferences _preferences;
-  // Maps route names to the corresponding _DemoStarsRecord, this map serves as
-  // a local cache of the cloud firestore snapshot.
-  final Map<String, _DemoStarsRecord> _demoStars = {};
-
-  @override
-  void initState() {
-    super.initState();
-    SharedPreferences.getInstance()
-      ..then((prefs) {
-        setState(() => this._preferences = prefs);
-      });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection('demo_stars').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            _loadDemoStarsFromSnapshot(snapshot);
-          }
-          final listTiles =
-              kMyAppRoutesStructure.map(_myRouteGroupToExpansionTile).toList()
-                ..add(
-                  _myRouteToListTile(kAboutRoute, leading: Icon(Icons.info)),
-                );
-          return ListView(children: listTiles);
-        });
+    final listTiles =
+        kMyAppRoutesStructure.map(_myRouteGroupToExpansionTile).toList()
+          ..add(
+            _myRouteToListTile(kAboutRoute, leading: Icon(Icons.info)),
+          );
+    return ListView(children: listTiles);
+    // return StreamBuilder<QuerySnapshot>(
+    //     stream: Firestore.instance.collection('demo_stars').snapshots(),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.hasData) {
+    //         _loadDemoStarsFromSnapshot(snapshot);
+    //       }
+    //       final listTiles =
+    //           kMyAppRoutesStructure.map(_myRouteGroupToExpansionTile).toList()
+    //             ..add(
+    //               _myRouteToListTile(kAboutRoute, leading: Icon(Icons.info)),
+    //             );
+    //       return ListView(children: listTiles);
+    //     });
   }
 
   Widget _myRouteGroupToExpansionTile(MyRouteGroup myRouteGroup) {
@@ -89,89 +80,22 @@ class _HomePageState extends State<HomePage> {
 
   // Returns the default leading widget of a ListTile: star icon with counts.
   Widget _getDefaultLeadingWidget(MyRoute route) {
+    // return null;
+    final state = MyRoute.of(context);
     return Stack(
       alignment: AlignmentDirectional.bottomCenter,
       children: <Widget>[
         IconButton(
-          icon: route.isStared(this._preferences)
+          icon: state.isStared(route.routeName)
               ? Icon(Icons.star, color: Theme.of(context).primaryColor)
               : Icon(Icons.star_border),
-          onPressed: () => _toggleStared(route),
+          onPressed: () async => await state.toggleStaredAsync(route),
         ),
         Text(
-          '${this._demoStars[route.routeName] != null ? this._demoStars[route.routeName].stars : 0}',
+          '${state.demoStars[route.routeName] != null ? state.demoStars[route.routeName].stars : 0}',
           style: Theme.of(context).textTheme.caption,
         ),
       ],
     );
   }
-
-  // Caches the value of demo stars from snapshot into this._demoStars.
-  void _loadDemoStarsFromSnapshot(AsyncSnapshot<QuerySnapshot> snapshot) {
-    for (DocumentSnapshot doc in snapshot.data.documents) {
-      final demoStarsRecord = _DemoStarsRecord.fromSnapshot(doc);
-      this._demoStars[demoStarsRecord.routeName] = demoStarsRecord;
-    }
-  }
-
-  // Creates a document corresponding to routeName on cloud firestore.
-  Future<Null> _createDemoStarsFirestoreDocument(String routeName) async {
-    // In firestore, the documents are keyed by corresponding route names
-    // (without the leading "/")
-    final documentKey = routeName.replaceAll('/', '');
-    await Firestore.instance
-        .collection('demo_stars')
-        .document(documentKey)
-        .setData({'routeName': routeName, 'stars': 0}, merge: true);
-  }
-
-  // Toggles the star status of one record, also updats the star counts in
-  // cloud firestore.
-  // If the corresponding document is not yet created on firestore, create it
-  // first, then re-call this function after 1 second.
-  void _toggleStared(MyRoute route) {
-    _DemoStarsRecord record = this._demoStars[route.routeName];
-    if (record == null) {
-      _createDemoStarsFirestoreDocument(route.routeName)
-        ..then((_) async {
-          // After the document is created, wait for the firestore update to be
-          // synced back -- 1 second should be more than enough.
-          await Future.delayed(Duration(seconds: 1));
-          // Next time this function is called, the record will not be null.
-          _toggleStared(route);
-        });
-      return;
-    }
-    int deltaStars = route.isStared(this._preferences) ? -1 : 1;
-    Firestore.instance.runTransaction((transaction) async {
-      final freshSnapshot = await transaction.get(record.firestoreDocReference);
-      final freshRecord = _DemoStarsRecord.fromSnapshot(freshSnapshot);
-      await transaction.update(record.firestoreDocReference,
-          {'stars': freshRecord.stars + deltaStars});
-    });
-    setState(() {
-      route.toggleStared(this._preferences);
-    });
-  }
-}
-
-// Custom data class for holding "{routeName,stars}" records.
-class _DemoStarsRecord {
-  final String routeName;
-  final int stars;
-  // Reference to this record as a firestore document.
-  final DocumentReference firestoreDocReference;
-
-  _DemoStarsRecord.fromMap(Map<String, dynamic> map,
-      {@required this.firestoreDocReference})
-      : assert(map['routeName'] != null),
-        assert(map['stars'] != null),
-        routeName = map['routeName'],
-        stars = map['stars'];
-
-  _DemoStarsRecord.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, firestoreDocReference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$routeName:$stars>";
 }
