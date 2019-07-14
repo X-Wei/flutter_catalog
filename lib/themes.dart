592cad85c8710cebb0867967bd06192cee50b148
x-wei <wistar.sjtu@gmail.com>
2019-07-14 17:34:22 +0200

    add light and dark themes

---
+++ lib/themes.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+// This file is adapted from
+// https://github.com/flutter/flutter/blob/master/examples/flutter_gallery/lib/gallery/themes.dart
+
+final kLightTheme = _buildLightTheme();
+final kDartTheme = _buildDarkTheme();
+
+ThemeData _buildLightTheme() {
+  const Color primaryColor = Color(0xFF0175c2);
+  const Color secondaryColor = Color(0xFF13B9FD);
+  final ColorScheme colorScheme = const ColorScheme.light().copyWith(
+    primary: primaryColor,
+    secondary: secondaryColor,
+  );
+  final ThemeData base = ThemeData(
+    brightness: Brightness.light,
+    accentColorBrightness: Brightness.dark,
+    colorScheme: colorScheme,
+    primaryColor: primaryColor,
+    buttonColor: primaryColor,
+    indicatorColor: Colors.white,
+    toggleableActiveColor: const Color(0xFF1E88E5),
+    splashColor: Colors.white24,
+    splashFactory: InkRipple.splashFactory,
+    accentColor: secondaryColor,
+    canvasColor: Colors.white,
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    errorColor: const Color(0xFFB00020),
+    buttonTheme: ButtonThemeData(
+      colorScheme: colorScheme,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+  return base.copyWith(
+    textTheme: base.textTheme,
+    primaryTextTheme: base.primaryTextTheme,
+    accentTextTheme: base.accentTextTheme,
+  );
+}
+
+ThemeData _buildDarkTheme() {
+  const Color primaryColor = Color(0xFF0175c2);
+  const Color secondaryColor = Color(0xFF13B9FD);
+  final ColorScheme colorScheme = const ColorScheme.dark().copyWith(
+    primary: primaryColor,
+    secondary: secondaryColor,
+  );
+  final ThemeData base = ThemeData(
+    brightness: Brightness.dark,
+    accentColorBrightness: Brightness.dark,
+    primaryColor: primaryColor,
+    primaryColorDark: const Color(0xFF0050a0),
+    primaryColorLight: secondaryColor,
+    buttonColor: primaryColor,
+    indicatorColor: Colors.white,
+    toggleableActiveColor: const Color(0xFF6997DF),
+    accentColor: secondaryColor,
+    canvasColor: const Color(0xFF202124),
+    scaffoldBackgroundColor: const Color(0xFF202124),
+    backgroundColor: const Color(0xFF202124),
+    errorColor: const Color(0xFFB00020),
+    buttonTheme: ButtonThemeData(
+      colorScheme: colorScheme,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+  return base.copyWith(
+    textTheme: base.textTheme,
+    primaryTextTheme: base.primaryTextTheme,
+    accentTextTheme: base.accentTextTheme,
+  );
+}
