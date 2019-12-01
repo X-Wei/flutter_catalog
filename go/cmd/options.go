589b4beb45ed013cad35d9139079ba100edf3590
x-wei <wistar.sjtu@gmail.com>
2019-12-01 11:06:26 +0100

    add go-flutter-desktip plugins

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/plugins/image_picker"
+	"github.com/go-flutter-desktop/plugins/path_provider"
+	"github.com/go-flutter-desktop/plugins/shared_preferences"
+	"github.com/go-flutter-desktop/plugins/url_launcher"
+)
+
+const (
+	kOrganization = "x_wei.github.io"
+	kAppname = "flutter_catalog"
 )
 
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(800, 1280),
+	flutter.AddPlugin(&image_picker.ImagePickerPlugin{}),
+	flutter.AddPlugin(&path_provider.PathProviderPlugin{
+		VendorName:      kOrganization,
+		ApplicationName: kAppname,
+	}),
+	flutter.AddPlugin(&shared_preferences.SharedPreferencesPlugin{
+		VendorName:      kOrganization,
+		ApplicationName: kAppname,
+	}),
+	flutter.AddPlugin(&url_launcher.UrlLauncherPlugin{}),
 }
--- go/go.mod
+++ go/go.mod
@@ -4,5 +4,9 @@ go 1.13
 
 require (
 	github.com/go-flutter-desktop/go-flutter v0.33.0
+	github.com/go-flutter-desktop/plugins/image_picker v0.1.5
+	github.com/go-flutter-desktop/plugins/path_provider v0.3.3
+	github.com/go-flutter-desktop/plugins/shared_preferences v0.4.3
+	github.com/go-flutter-desktop/plugins/url_launcher v0.1.2
 	github.com/pkg/errors v0.8.1
 )
