62ff87b8a584d51853dfcae0353c1257b0d0eeed
x-wei <wistar.sjtu@gmail.com>
2020-09-27 15:15:43 +0200

    v2.5.5+57

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // *Note*: when APP_VERSION is changed, remember to also update pubspec.yaml.
-const APP_VERSION = 'v2.5.3';
+const APP_VERSION = 'v2.5.5';
 const APP_NAME = 'Flutter Catalog';
 final kAppIcon =
     Image.asset('res/images/launcher_icon.png', height: 64.0, width: 64.0);
