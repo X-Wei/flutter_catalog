468be67bd5404816a3005b4d0a504b3cfe86baac
x-wei <wistar.sjtu@gmail.com>
2020-09-27 09:10:08 +0200

    #fixup test file -- it's still a dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_catalog/main.dart';
+import 'package:flutter_catalog/my_main_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyMainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
