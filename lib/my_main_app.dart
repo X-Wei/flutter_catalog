678b35b871a041aceb3eb291daa9781e08bb8e16
x-wei <wistar.sjtu@gmail.com>
2021-01-17 14:58:46 +0100

    v2.6.1+69

--- lib/my_main_app.dart
+++ lib/my_main_app.dart
@@ -37,7 +37,7 @@ class _MyMainAppState extends State<MyMainApp> {
       future: this._initAppFuture,
       builder:
           (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
-        print('snapshot=$snapshot');
+        // print('snapshot=$snapshot');
         if (!snapshot.hasData) {
           return const _MySplashScreen();
         }
