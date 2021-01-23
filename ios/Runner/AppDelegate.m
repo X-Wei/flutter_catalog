b09f72edb55ffc858bf34232653cef0a0f07b8fb
x-wei <wistar.sjtu@gmail.com>
2021-01-23 23:37:36 +0100

    add native splash screen
    $ flutter pub run flutter_native_splash:create

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,9 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    int flutter_native_splash = 1;
+    UIApplication.sharedApplication.statusBarHidden = false;
+
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
