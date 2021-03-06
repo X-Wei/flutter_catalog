bf75449d726083b55a9b2bbcbcfc2bcfbbd96376
x-wei <wistar.sjtu@gmail.com>
2021-03-06 18:12:02 +0100

    try to fix the MissingPluginException pb
    https://github.com/flutter/flutter/issues/65334#issuecomment-787657641

--- android/app/src/main/kotlin/io/github/x_wei/flutter_catalog/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/x_wei/flutter_catalog/MainActivity.kt
@@ -3,4 +3,7 @@ package io.github.x_wei.flutter_catalog
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine) // add this line
+  }
 }
