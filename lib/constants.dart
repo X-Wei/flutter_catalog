ac3f694ac0627bcdcbc1aa70b5d06d640cd76e3d
x-wei <wistar.sjtu@gmail.com>
2021-03-06 19:46:16 +0100

    add kPlatformType
    because some demo doesn't work on certain platforms

--- lib/constants.dart
+++ lib/constants.dart
@@ -21,3 +21,41 @@ const AUTHOR_SITE = 'http://x-wei.github.io';
 // Whether the app is running on mobile phones (Android/iOS)
 // ! `Platform` is not available on web !
 final kIsOnMobile = kIsWeb || Platform.isAndroid || Platform.isIOS;
+
+/// Adapted from https://www.flutterclutter.dev/flutter/tutorials/how-to-detect-what-platform-a-flutter-app-is-running-on/2020/127/
+
+final kPlatformType = getCurrentPlatformType();
+
+enum PlatformType { Web, iOS, Android, MacOS, Fuchsia, Linux, Windows, Unknown }
+
+PlatformType getCurrentPlatformType() {
+  if (kIsWeb) {
+    return PlatformType.Web;
+  }
+
+  if (Platform.isMacOS) {
+    return PlatformType.MacOS;
+  }
+
+  if (Platform.isFuchsia) {
+    return PlatformType.Fuchsia;
+  }
+
+  if (Platform.isLinux) {
+    return PlatformType.Linux;
+  }
+
+  if (Platform.isWindows) {
+    return PlatformType.Windows;
+  }
+
+  if (Platform.isIOS) {
+    return PlatformType.iOS;
+  }
+
+  if (Platform.isAndroid) {
+    return PlatformType.Android;
+  }
+
+  return PlatformType.Unknown;
+}
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,7 +8,6 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -23,7 +22,6 @@ void registerPlugins(Registrar registrar) {
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
-  FirebaseStorageWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
