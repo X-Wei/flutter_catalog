32fdcae3a1a3f3a396dd19dda8f734192611aa42
x-wei <wistar.sjtu@gmail.com>
2021-03-06 18:12:10 +0100

    v2.6.3

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 // ignore_for_file: constant_identifier_names
 
 // *Note*: when APP_VERSION is changed, remember to also update pubspec.yaml.
-const APP_VERSION = 'v2.6.2';
+const APP_VERSION = 'v2.6.3';
 const APP_NAME = 'Flutter Catalog';
 final kAppIcon =
     Image.asset('res/images/launcher_icon.png', height: 64.0, width: 64.0);
