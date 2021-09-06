faabcaa09b0767ca6d426d268051b92636049ebf
dasa376 <83995377+dasa376@users.noreply.github.com>
2021-09-06 14:25:37 +0700

    Update async_futurebuilder_ex.dart

--- lib/routes/async_futurebuilder_ex.dart
+++ lib/routes/async_futurebuilder_ex.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 import 'package:flutter/services.dart';
 
 class FutureBuilderExample extends StatefulWidget {
