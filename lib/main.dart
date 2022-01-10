0d183f932c53ec6926abbcb6e24062847a0a80e0
x-wei <wistar.sjtu@gmail.com>
2022-01-10 20:57:24 +0100

    add firebase config using flutterfire CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import './constants.dart';
+import './firebase_options.dart';
 import './my_app_settings.dart';
 import './my_main_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsOnMobile) {
-    await Firebase.initializeApp();
+  if (kIsOnMobile || kIsWeb) {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
   }
   final settings = await MyAppSettings.create();
   runApp(MyMainApp(settings));
